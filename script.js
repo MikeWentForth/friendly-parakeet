fa2dc17391d408dbf8a15562f2d9f5c68a23abce
MikeWentForth <themikewallace@gmail.com>
2023-07-05 23:13:46 -0700

    finalized project with js

--- script.js
+++ script.js
@@ -7,132 +7,104 @@ var choiceArray = [];
 var specialCharArray = ['!', '@', '#', '$', '%', '^', '&', '*'];
 var lowerCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'X'];
-var numberArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+var numberArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = passwordGenerator();
-  var passwordText = document.getElementById("password");
-  passwordText.value = password;
-}
+var generateBtn = document.querySelector("#generate");
+
 
 function generatePrompts() {
-  //I would generate password based on the prompts//
 
   // here we are requesting data from the USER
   var length = window.prompt("Enter password length between 8 and 128 characters");
   // now we VALIDATE the USER input
+  console.log('length' + length)
   if (length > 128 || length < 8) {  // --> '10' > 128   --> 10 > 128 --> true || false
-    alert("Password must be greater than 8 and less than 128 character");
+    alert("Password must be greater than 8 and less than 128 characters");
     return;  // anytime we see a RETURN statment we ExIT the function!!
   }
 
   var lowercase = window.confirm("Click to include lowercase letters.");  // --> return either TRUE or FALSE 
   if (lowercase) {
-    var lowerData = lowerCaseArray.join('');
-    console.log(lowerData);
-    console.log(typeof lowerData);
-    choiceArray.push(lowerData);
+
+
+    choiceArray = choiceArray.concat(lowerCaseArray);
   }
 
   var uppercase = window.confirm("Click to include uppercase letters.");
   if (uppercase) {
-    var upperData = upperCaseArray.join('');
-    console.log(upperData);
-    console.log(typeof upperData);
-    choiceArray.push(upperData);
+
+    choiceArray = choiceArray.concat(upperCaseArray);
 
   }
 
   var numeric = window.confirm("Click to include numeric characters?");
   if (numeric) {
-    var numberData = numberArray.join('');
-    console.log(numberData);
-    console.log(typeof numberData);
-    choiceArray.push(numberData);
+
+    choiceArray = choiceArray.concat(numberArray);
 
   }
 
   var specialchar = window.confirm("Click to include special characters?");
   if (specialchar) {
-    var specialCharData = specialCharArray.join('');
-    console.log(specialCharData);
-    console.log(typeof specialCharData);
-    choiceArray.push(specialCharData);
+
+    choiceArray = choiceArray.concat(specialCharArray);
   }
 
   console.log("Dataset: ", choiceArray)
 
-  passwordGenerator(choiceArray);
-  return choiceArray;
+
+
+ return passwordGenerator(choiceArray, length);
+
 }
 
-function passwordGenerator(dataset) {
 
-  var Dataset = 'choiceArray';
 
-  create_random_string()
-  function create_random_string(string_length) {
-    var random_string = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*'
-    for (var i, i = 0; i < 128; i++) {
-      random_string += characters.charAt(Math.floor(Math.random() * characters.length))
-    }
-    console.log(random_string)
+function passwordGenerator(possibleOptions, passwordLength) {
+
+  var userPassword = '';
+
+
+
+
+
+
+  for (var i = 0; i < passwordLength; i++) {
+    var index = Math.floor(Math.random() * possibleOptions.length);
+
+    console.log('index; ' + index);
+
+    userPassword += possibleOptions[index]; userPassword
+    console.log('userPassword: ' + userPassword);
+    console.log('iteration ' + i)
   }
 
+  console.log(userPassword)
+
+  return userPassword;
 
 
 
-
-
-  return choiceArray;
 }
 
+function writePassword() {
+var password = generatePrompts();
+var PasswordText = document.querySelector("#password");
+
+PasswordText.value = password;
+
+
+}
+
+generateBtn.addEventListener("click", writePassword);  
+
+
+// (password)(pwLength, isLowercase, isUppercase, isNumeric, isSpecialchar)
 
 
 
 
-  // Then we need to run logic 
-
-  // FROM our gatherd dataset
-  // --> Keep in mind HOW MANY CHARACTERS are going to be in the password(?)
-  // --> HOW can we randomly choose a CHARACTER from our allowed Dataset(?)
-  // --> REMEMBER to RETURN the password from the function
-
-
-  //(password)(pwLength, isLowercase, isUppercase, isNumeric, isSpecialchar)
-
-
-
-// function getPrompts() {
-
-//   characterLength = parseInt(prompt("how many characters do you want your password to be? (8 - 128 characters")); //NaN 
-
-//   if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
-//     alert("Character length has to be a number, 8 - 128 digits. Please try again.");
-//     return false;
-//   }
-//   if (confirm("would you like lower case letters in your password?")) {
-//     choiceArr = choiceArr.concat(lowerCassArr);
-//   }
-
-//   if (confirm("would you like upper case letters in your password?")) {
-//     choiceArr = choiceArr.concat(upperCassArr);
-//   }
-
-//   if (confirm("would you like special case letters in your password?")) {
-//     choiceArr = choiceArr.concat(specialCharArr);
-
-//     if (confirm("would you like upper case letters in your password?")) {
-//       choiceArr = choiceArr.concat(numberArr);
-//     }
-
-//     return true;
-
-//   }
-// }
 
 
 
